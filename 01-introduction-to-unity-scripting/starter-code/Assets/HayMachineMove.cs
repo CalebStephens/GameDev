f6756f5044959b62867b99a4f60ced968549b13d
CalebStephens <calebtaylorstephens@gmail.com>
2022-03-16 13:49:27 +1300

    Sound applied when shooting dying falling

--- 01-introduction-to-unity-scripting/starter-code/Assets/HayMachineMove.cs
+++ 01-introduction-to-unity-scripting/starter-code/Assets/HayMachineMove.cs
@@ -52,5 +52,6 @@ public class HayMachineMove : MonoBehaviour
     private void ShootHay()
     {
         Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+        SoundManager.Instance.PlayShootClip();
     }
 }
