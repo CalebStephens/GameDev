4bef0517d58ffc94205d63bccf5ddc184fff31b0
CalebStephens <calebtaylorstephens@gmail.com>
2022-06-10 14:54:27 +1200

    randomization

--- 04-ai-strategy/starter code/Assets/Scripts/Minimax.cs
+++ 04-ai-strategy/starter code/Assets/Scripts/Minimax.cs
@@ -150,6 +150,7 @@ public class Minimax : MonoBehaviour
         {
             int maxScore = int.MinValue;
             List<MoveData> allMoves = GetMoves(gameManager.playerTurn);
+            allMoves = Shuffle(allMoves);
             foreach (MoveData move in allMoves)
             {
                 moveStack.Push(move);
@@ -174,6 +175,7 @@ public class Minimax : MonoBehaviour
             PlayerTeam opponent = gameManager.playerTurn == PlayerTeam.WHITE ? PlayerTeam.BLACK : PlayerTeam.WHITE;
             int minScore = int.MaxValue;
             List<MoveData> allMoves = GetMoves(opponent);
+            allMoves = Shuffle(allMoves);
             foreach (MoveData move in allMoves)
             {
                 moveStack.Push(move);
@@ -187,5 +189,18 @@ public class Minimax : MonoBehaviour
             }
             return minScore;
         }
+    }
+    
+    public List<T> Shuffle<T>(List<T> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = Random.Range(0,n);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+        return list;
     }
 }
